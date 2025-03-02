cdbdaa2aef80c51941328a35d2d28cfe284592af
Yassine Gherbi <y.gherbi.dev@gmail.com>
2025-03-02 23:17:46 +0100

    feat: add manifest file and update Vite configuration for YouTube Studio AI extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/manifest.json',
+          dest: '.',
+        }
+      ],
+    }),
+  ],
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        content: './src/content.ts',
+        background: './src/background.ts'
+      },
+      output: {
+        entryFileNames: '[name].js'
+      }
+    },
+  },
+});
